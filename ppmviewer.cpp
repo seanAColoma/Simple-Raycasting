1c259e5cc0688bf04e049b155417fcb391915ff3
Sean Coloma <sac27@sfu.ca>
2024-10-30 17:17:10 -0700

    first commit

---
+++ ppmviewer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    int img_width = 256;
+    int img_height = 256;
+    int maxColor = 255;
+    //P3 (header)
+    //256, 256 (image dimensions)
+    //255 (maximum color intensity)
+    std::cout << "P3\n" << img_width << " " << img_height << "\n" << maxColor << "\n";
+
+    //image values
+    for(int j = 0; j<img_height; j++){
+        //progress reporting
+        for(int i = 0; i<img_width; i++){
+            auto r = double(i)/maxColor;
+            auto g = double(j)/maxColor;
+            auto b = 0.0;
+
+            //scale the rgb values for printing to ppm
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << " " << ig << " " << ib << "\n"; 
+
+        }
+    }
+}
