a982598f9ab48f3d095d14ea9a3f7b879e1792b2
Sean Coloma <sac27@sfu.ca>
2024-11-02 02:34:56 -0700

    added rendering for a sphere shaded with surface normals colormapped based on component values

--- ppmviewer.cpp
+++ ppmviewer.cpp
@@ -3,13 +3,34 @@
 #include "vec3.h"
 #include "ray.h"
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0*dot(r.direction(), (oc));
+    auto c = dot(oc,oc) - (radius * radius);
+    auto discriminant = b*b - 4*a*c;
+    
+    if(discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        //we use the - discriminant form since we want the smallest t ie the first time the ray hits the sphere
+        return(-b - std::sqrt(discriminant)) / (2.0*a);
+    }
+}
 //returns the color of a ray
 color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if(t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x() + 1, N.y()+1, N.z()+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     //linear interpolation based on y coordinate to create white to blue gradient
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5, 0.7, 1.0);
 }
+
 
 int main() {
     int maxColor = 255;
