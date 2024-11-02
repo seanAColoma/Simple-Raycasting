2743c9f05b40c175bbc76af7d34ee92bcfb58aca
Sean Coloma <sac27@sfu.ca>
2024-11-02 02:04:16 -0700

    rewrote main function to use rays and viewport - now generates gradient image based on y coordinate

--- ppmviewer.cpp
+++ ppmviewer.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
+
+//returns the color of a ray
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    //linear interpolation based on y coordinate to create white to blue gradient
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5, 0.7, 1.0);
+}
 
 int main() {
-    int img_width = 256;
-    int img_height = 256;
     int maxColor = 255;
-    
+    auto aspect_ratio = 16.0/9.0;
+    //img width, height must be natural numbers - they define the number of pixels along each axiss
+    int img_width = 400;
+    int img_height = int(img_width/aspect_ratio);
+    //make sure img_height is at least 1
+    img_height = (img_height < 1) ? 1 : img_height;
+
+    //camera
+    auto focal_length = 1.0;
+    //viewport height, width are real values - they define the size of a plane (the viewport) in 3d space
+    auto viewport_height = 2.0;
+    auto viewport_width  = viewport_height * (double(img_width)/img_height);
+    auto camera_center = point3(0,0,0);
+
+    //the vector in 3d space along the horizontal edge
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    //the vector in 3d space along the vertical edge (pointing downwards)
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    //delta vectors: vector inbetween pixels
+    auto pixel_delta_u = viewport_u/img_width;
+    auto pixel_delta_v = viewport_v/img_height;
+
+    //calculate the location of the upper left corner of the viewport
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+    //calculate the location (in 3d space) of the upper left pixel (in the image) relative to the camera
+    //outer edge of pixels 0.5 distance away from edge of viewport
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     //P3 (header)
-    //256, 256 (image dimensions)
+    //img_width, img_height (image dimensions)
     //255 (maximum color intensity)
     std::cout << "P3\n" << img_width << " " << img_height << "\n" << maxColor << "\n";
 
@@ -17,7 +51,11 @@ int main() {
         //progress reporting
         std::clog << "\rScanlines Remaining: " << (img_height - 1) << ' ' << std::flush;
         for(int i = 0; i<img_width; i++){
-            color pixel_color = color(double(i)/(img_width-1), double(j)/(img_height-1), 0);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
---
+++ ray.h
@@ -0,0 +1,24 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+    public:
+        ray() {}
+        ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+        
+        //return a const reference and function itself is const - can't change members within function
+        const point3& origin() const { return orig; }
+        const vec3& direction() const { return dir; }
+
+        //P(t) function - moves point along ray defined by Ab (origin * direction) by factor t
+        point3 at(double t) const {
+            return orig + t*dir;
+        }
+
+    private:
+        point3 orig;
+        vec3 dir;
+};
+#endif
